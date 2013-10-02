9dec151edd15b721fe2f35f42a03de0375e208cd
janevalenti <jvalen1@students.towson.edu>
2013-10-01 18:21:33 -0700

    Initial creation of the project skeleton.

---
+++ Project/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project::Application.config.secret_token = '8340da2364eb5bf54e5492011b825a0915fb21da05009d2d88468c43ecaf0ba7e7400242d5386eb132d041822d6a783171c9a7d3b5442e50c0148870200b8656'
