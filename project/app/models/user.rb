b76f1c2130178e8a5df3eed0e49430bca188b232
Manny Rivera <mcomp2010@yahoo.com>
2013-11-15 21:07:23 -0500

    Modified some user pages

--- project/app/models/user.rb
+++ project/app/models/user.rb
@@ -25,6 +25,30 @@ class User < ActiveRecord::Base
 
   has_many :badges
 
+  def update_with_password(params={})
+    current_password = params.delete(:current_password)
+
+    if params[:password].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation) if params[:password_confirmation].blank?
+    end
+
+    #result = if valid_password?(current_password)
+    result = if params[:password].blank? && if params[:password].blank? || valid_password?(current_password)
+               update_attributes(params)
+             else
+               self.attributes = params
+               self.valid?
+               self.errors.add(:current_password, current_password.blank? ? :blank : :invalid)
+               false
+             end
+
+            clean_up_passwords
+            result
+             end
+  end
+
+
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
     user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
