39ce16b46dbd850c1f802ef113114cee6b8afe01
Manny Rivera <mcomp2010@yahoo.com>
2013-11-21 23:45:25 -0500

    Form Validation part 2

--- project/app/assets/javascripts/forms_validations.js
+++ project/app/assets/javascripts/forms_validations.js
@@ -1,7 +1,46 @@
-/**
- * Created with JetBrains RubyMine.
- * User: user
- * Date: 11/21/13
- * Time: 11:35 PM
- * To change this template use File | Settings | File Templates.
- */
+(function($,W,D)
+{
+    var JQUERY4U = {};
+
+    JQUERY4U.UTIL =
+    {
+        setupFormValidation: function()
+        {
+            //form validation rules
+            $("#new_user").validate({
+                rules: {
+                    username: "required",
+
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    user_password: {
+                        required: true,
+                        minlength: 5
+                    }
+
+                },
+                messages: {
+                    username: "Please enter your username",
+
+                    user_password: {
+                        required: "Please provide a password",
+                        minlength: "Your password must be at least 5 characters long"
+                    },
+                    email: "Please enter a valid email address"
+
+                },
+                submitHandler: function(form) {
+                    form.submit();
+                }
+            });
+        }
+    }
+
+    //when the dom has loaded setup form validation rules
+    $(D).ready(function($) {
+        JQUERY4U.UTIL.setupFormValidation();
+    });
+
+})(jQuery, window, document);
